85d1d5c549b735c66ced86917e678d990167221d
Tharani-Kasiselvam <sherley.tharani@gmail.com>
2025-04-01 12:27:00 +0530

    same site updated as None

--- contorllers/ModuleControllers.js
+++ contorllers/ModuleControllers.js
@@ -88,7 +88,8 @@ export const login = async (req,res)=>{
         res.cookie("token", token, {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production",
-          sameSite: "strict",
+          // sameSite: "strict",
+          sameSite: "None",
           maxAge: 7 * 24 * 60 * 60 * 1000,
         });
       }
